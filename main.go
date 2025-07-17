0cca9d46c716751d07ce3477670820832b455dc6
Thomika1 <thomazmbonfim@gmail.com>
2025-07-16 21:32:06 -0300

    Feature: Env added

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"github.com/Thomika1/TestOrga/database"
 	"github.com/Thomika1/TestOrga/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	godotenv.Load()
+}
 func main() {
 
 	r := gin.Default()
