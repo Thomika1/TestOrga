7632ad557efccac668b5fa4972498727bd3f4ac9
Thomika1 <thomazmbonfim@gmail.com>
2025-07-18 17:41:40 -0300

    Feature: added topics for exam model

--- model/exams.go
+++ model/exams.go
@@ -4,13 +4,15 @@ type Exam struct {
 	ID        int    `json:"id"`
 	UserID    int    `json:"user_id"`
 	Subject   string `json:"subject"`
-	ExamDate  string `json:"exam_date"` // formato "2006-01-02"
+	Topics    string `json:"topics"` // formato "2006-01-02"
+	ExamDate  string `json:"exam_date"`
 	CreatedAt string `json:"created_at"`
 }
 
 type ExamResponse struct {
 	ID        int    `json:"id"`
 	Subject   string `json:"subject"`
+	Topics    string `json:"topics"`
 	ExamDate  string `json:"exam_date"` // formato "YYYY-MM-DD"
 	CreatedAt string `json:"created_at"`
 }
