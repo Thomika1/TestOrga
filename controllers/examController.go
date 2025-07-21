2faa1e9bd3d62124892a490a4a6125312e5733d6
Thomika1 <thomazmbonfim@gmail.com>
2025-07-21 20:51:26 -0300

    Feature: added get exam list per user: controller

--- controllers/examController.go
+++ controllers/examController.go
@@ -50,3 +50,17 @@ func (e *examController) RegisterExam(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "Exam inserted", "exam": examResponse})
 
 }
+
+func (e *examController) GetExams(ctx *gin.Context) {
+	user_id, exists := ctx.Get("user_id")
+	if !exists {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "usuário não autenticado"})
+		return
+	}
+
+	exams, err := e.examUsecase.GetExams(user_id.(int))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Exams selected", "exams": exams})
+}
